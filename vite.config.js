838b9dcc6d71fb9a8d43642765ec81d081be160c
Daniel Ramírez <jdramirez00@ucatolica.edu.co>
2025-04-30 02:40:41 -0500

    fix: ajustes finales para despliegue y configuración

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/LibreriaFrontend/',
   plugins: [react()],
   server: {
-    port: 5174, // Cambia este número al puerto que desees
+    port: 5174,
   },
 });
